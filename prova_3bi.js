d15cb4f0721dcad8813b6aee66f60d869ef16b65
rafaelminski <rafael.minski57@gmail.com>
2023-09-20 01:17:13 -0300

    atualização prova

--- prova_3bi.js
+++ prova_3bi.js
@@ -4,21 +4,43 @@ const respLista = document.querySelector("pre");
 
 const numeros = []; // declara um vetor global
 
+  
 frm.addEventListener("submit", (e) => {
     e.preventDefault();
     const numero = frm.inNumero.value;
     numeros.push(numero);
-
+    
     let lista = "";
-
-    for (let i =0;i< numeros.length; i++){
-        if(numeros[i]> numeros[i+1]){
-        lista+=`${numeros [i]}\n`
-        }else{
-        lista+=`${numeros [i]}\n`
-        }
+    function compararNumeros(a, b) {
+        return a - b;
+      }
+    for ( let i =0;i< numeros.length; i++){
+        
+        numeros.sort(function (a, b) {
+            return a - b;
+           
+          });
+        
+          lista+=`${numeros[i]}\n`
     }
     respLista.innerText = lista;
     frm.inNumero.value = "";
     frm.inNumero.focus();
+})
+
+frm.btnRemover.addEventListener("click", () => {
+    if(numeros.length==0){
+        alert("não consta na lista")
+        
+        respNome.innerText="";
+        return
+    }
+    const Remover = numeros.shift();
+    respNome.innerText = Remover;
+    let lista = "";
+    numeros.forEach((numero, i ) => (lista +=`${ i + 1 }. ${numeros[i]}\n`));
+    respLista.innerText = lista;
+    
+  
+    
 })
